877df019c96d4c777c05c180bb32bb6ad54a1394
Michael Pappas <mixalis.pappas23@gmail.com>
2021-11-03 11:05:38 +1100

    ts. config

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
 let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Jack';
+if (typeof userInput === 'string') {
+  userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError('An error occurred!', 500);
