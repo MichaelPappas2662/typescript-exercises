b728e19ddf464ccefdc8e6f4e755f5d689d6a5a9
Michael Pappas <mixalis.pappas23@gmail.com>
2021-12-14 22:29:58 +1100

    getters and setters

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 class Department {
   // private id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
-  constructor(private id: string, public name: string) {
+  constructor(private readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
   }
@@ -23,17 +23,63 @@ class Department {
   }
 }
 
-const accounting = new Department('d1', 'Accounting');
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, 'IT');
+  }
+}
 
+class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found.');
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error('Please pass a value');
+    }
+    this.addReports(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+    this.lastReport = reports[0];
+  }
+
+  addEmployee(name: string) {
+    if (name === 'Max') {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReports(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment('d1', ['Accounting']);
+const accounting = new AccountingDepartment('d2', []);
+
+it.addEmployee('Max');
+it.addEmployee('Peter');
+
+it.describe();
+it.name = 'NEW NAME';
+it.printEmployeeInformation();
+
+accounting.mostRecentReport = 'Year End Report';
+accounting.addReports('Something went wrong ...');
+console.log(accounting.mostRecentReport);
 accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-
-// accounting.employees[2] = 'Anna';
-
-accounting.describe();
-accounting.name = 'NEW NAME';
-accounting.printEmployeeInformation();
-
-// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-
-// accountingCopy.describe();
+accounting.addEmployee('Peter');
